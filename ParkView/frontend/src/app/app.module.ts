7caa318ee8d1e6152fdafd428e4e3893b86eef02
Sairama-S <sairama.shrinath@gmail.com>
2023-09-02 11:46:15 +0530

    nav-bar component working

--- ParkView/frontend/src/app/app.module.ts
+++ ParkView/frontend/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './shared/nav/nav.component';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
