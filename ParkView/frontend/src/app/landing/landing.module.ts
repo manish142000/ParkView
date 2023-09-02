c5591d9be2ed18d8e31afb1000495517f749cd97
manish142000 <bawkarmanish14@email.com>
2023-09-02 11:59:28 +0530

    Created Landing Module

---
+++ ParkView/frontend/src/app/landing/landing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SectionAComponent } from './section-a/section-a.component';
+
+
+
+@NgModule({
+  declarations: [
+    SectionAComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class LandingModule { }
