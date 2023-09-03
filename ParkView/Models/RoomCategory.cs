9dc2f097c1683e9d7493e06a16b34073d96d5114
Sairama-S <sairama.shrinath@gmail.com>
2023-09-03 13:10:47 +0530

    Added RoomCategory class

---
+++ ParkView/Models/RoomCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace ParkView.Models
+{
+    public class RoomCategory
+    {
+        public string CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public int DailyRate { get; set; }
+    }
+}
