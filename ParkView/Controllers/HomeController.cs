994cce0ff71f27b39e75ce4dd9f27ed5f151c231
manish142000 <bawkarmanish14@email.com>
2023-09-04 12:47:28 +0530

    random commit

--- ParkView/Controllers/HomeController.cs
+++ ParkView/Controllers/HomeController.cs
@@ -8,15 +8,50 @@ namespace ParkView.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IRoom _room;
+
+        private readonly IRoomCategory _roomCategory;
+
+        private readonly IHotel _hotel;
+
+        public HomeController(ILogger<HomeController> logger, 
+            IRoom room, IRoomCategory roomCategory, IHotel hotel)
         {
             _logger = logger;
+            _room = room;
+            _roomCategory = roomCategory;
+            _hotel = hotel;
         }
 
         public IActionResult Index()
         {
             return View();
         }
+        
+        [HttpPost]
+        public IActionResult Index(SearchForm form)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                Console.WriteLine("its coming");
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        //[HttpPost]
+        //public RedirectToActionResult SearchForms([FromBody] SearchForm form)
+        //{
+        //    Console.WriteLine(form.check_in);
+        //    Console.WriteLine(form.check_out);
+        //    Console.WriteLine(form.adultCount);
+        //    Console.WriteLine(form.childrenCount);
+
+        //    return RedirectToAction("Index");
+        //}
+
 
         public IActionResult Privacy()
         {
---
+++ ParkView/Controllers/RoomController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ParkView.Controllers
+{
+    public class RoomController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
